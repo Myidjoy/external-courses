ca5f7b1ee3fd48074eb3797dc3869c5d36cdd4ca
Myidjoy <83170608+Myidjoy@users.noreply.github.com>
2021-10-09 19:30:00 +0300

    ex12 is done

---
+++ src/ex12_js_dom/inner.js
@@ -0,0 +1,42 @@
+const buttonPrev = document.querySelector('.slider__item-one');
+const buttonNext = document.querySelector('.slider__item-two');
+const contentElements = ['./assets/1.jpg', './assets/2.jpg', './assets/3.jpg', './assets/4.jpg', './assets/5.jpg', './assets/6.jpg'];
+const containerElement = document.querySelector('.container__content__elem');
+let index = 1;
+
+function clearPageContents() {
+  while (document.querySelector('.container__content__elem').childElementCount > 0) {
+    document.querySelector('.container__content__elem').firstChild.remove();
+    containerElement.classList.remove('open');
+  }
+}
+function showPicture(n) {
+  clearPageContents();
+  setTimeout(() => {
+    if (n > contentElements.length) {
+      index = 1;
+    }
+
+    if (n < 1) {
+      index = contentElements.length;
+    }
+
+    const image = document.createElement('img');
+    image.src = contentElements[index - 1];
+
+    containerElement.append(image);
+    containerElement.classList.add('open');
+  }, 100);
+}
+function plusSlides(n) {
+  showPicture(index += n);
+}
+
+showPicture(index);
+
+buttonNext.addEventListener('click', () => {
+  plusSlides(1);
+});
+buttonPrev.addEventListener('click', () => {
+  plusSlides(-1);
+});
